f032d13962267a3dc15971af3eabbb948d66a5c9
ArturoDuCas <a00833516@tec.mx>
2023-07-03 12:53:07 -0600

    feat: implement api endpoint

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// index.js
-// where your node app starts
-
-// init project
+// Init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,11 +15,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}))
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/:date?", (req, res) => {
+  let inputDate = req.params.date;
+  if (!isNaN(inputDate)) inputDate = Number(inputDate); // When receiving unix timestamp
+
+  let dateObj;
+  if (inputDate === undefined) {
+    dateObj = new Date()
+  } else {
+    dateObj = new Date(inputDate);
+  }
+
+  if(isNaN(dateObj.getTime())) { // Invalid date
+    res.json({
+      error: "Invalid Date",
+    })
+  } else { // Valid date
+    let unixTime = dateObj.getTime();
+    let utcTime = dateObj.toUTCString();
+    res.json({
+      unix: unixTime,
+      utc: utcTime,
+    })
+  }
+})
 
 
 
